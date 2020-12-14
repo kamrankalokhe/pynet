4d709c710294cf0d882c3b6888a4da378f068f91
Kamran Kalokhe <kamrankalokhe15@gmail.com>
2020-12-13 20:53:59 -0800

    updating python scripts

--- test2.py
+++ test2.py
@@ -2,5 +2,5 @@ print('Hello World')
 print('Hello World')
 print('Hello World')
 print('Hello World')
-print('Hello World')
-print('Hello World')
+for j in range(2):
+    print('Hello World')
--- test3.py
+++ test3.py
@@ -1,2 +1,5 @@
 for i in range(10):
     print('Hello World =',i)
+
+for x in range(100):
+    print(x*x)
