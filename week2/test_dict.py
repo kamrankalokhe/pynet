ad917020e0caa44997320c79630f7ebb3603ea17
Kamran Kalokhe <kamrankalokhe15@gmail.com>
2021-11-25 08:57:40 -0800

    test2

---
+++ week2/test_dict.py
@@ -0,0 +1,19 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device1 = {'host':'cisco3.lasthop.io',
+           'username':'pyclass',
+           'password':getpass(),
+           'device_type':'cisco_ios'
+            }
+
+net_connect = ConnectHandler(**device1)
+print(net_connect.find_prompt())
+
+command = 'delet flash:/kam_test.txt'
+output = net_connect.send_command(command,expect_string=r'Delete filename',strip_prompt=False,strip_command=False)
+output += net_connect.send_command('y',expect_string=r'confirm',strip_prompt=False,strip_command=False)
+output += net_connect.send_command('y',expect_string=r'#',strip_prompt=False,strip_command=False)
+
+print(output)
+
